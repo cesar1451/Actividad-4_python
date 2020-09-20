a629f77d9113115ef6e78ab1420434382c6cb5a6
César González <cesar.gonzalez7978@alumnos.udg.mx>
2020-09-20 13:41:24 -0500

    Se agregaron las funciones para obtener areas de las figuras

---
+++ areaFiguras.py
@@ -0,0 +1,16 @@
+import math
+import os
+
+def areaCuadrado(lado):
+    return lado*lado
+
+def areaTriangulo(base, altura):
+    return (base*altura)/2
+
+def areaCirculo(radio):
+    return (math.pi * (pow(radio, 2)))
+
+
+        
+    os.system("pause")
+    os.system("cls")                
